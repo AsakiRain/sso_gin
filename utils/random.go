ceae970900bb601bf4e7b48ce5d1777bb68531b5
AsakiRain <1287059631@qq.com>
2022-10-02 00:58:39 +0800

    fix(captcha):修复验证码使用固定种子的问题

--- utils/random.go
+++ utils/random.go
@@ -2,14 +2,16 @@ package utils
 
 import (
 	"math/rand"
+	"time"
 )
 
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 func RandomCode(n int) string {
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
 	b := make([]byte, n)
 	for i := range b {
-		b[i] = letters[rand.Int63()%int64(len(letters))]
+		b[i] = letters[rnd.Int63()%int64(len(letters))]
 	}
 	return string(b)
 }
