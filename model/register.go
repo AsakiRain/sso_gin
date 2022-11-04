1ab29c8891b738586080a1d0cde580397e62c223
AsakiRain <1287059631@qq.com>
2022-11-04 14:36:14 +0800

    chore: 删除了不需要的model

--- model/register.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type RegisterForm struct {
-	Username string `json:"username" binding:"required"`
-	Nickname string `json:"nickname" binding:"required"`
-	Password string `json:"password" binding:"required"`
-	Email    string `json:"email" binding:"required"`
-	Code     string `json:"code" binding:"required"`
-}
